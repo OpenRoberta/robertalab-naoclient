57a21725418b8a8358fc165e077dc9f7518c6b16
gregory.vimont <gregory.vimont@softbankrobotics.com>
2020-11-21 19:25:16 +0100

    Fix _wait_for_service function's timeout
    The function returns after the first failure.
    It should return after the timeout.
    
    https://github.com/OpenRoberta/robertalab-naoclient/issues/3

--- oraclient-choreographe-app-v5/app/scripts/main.py
+++ oraclient-choreographe-app-v5/app/scripts/main.py
@@ -405,7 +405,7 @@ class OpenRobertaClient(object):
                 return True
             except RuntimeError:
                 time.sleep(1.0)
-                return
+        return
         # Failed, give up
 
     def on_start(self):
